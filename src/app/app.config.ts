1089720bf99421947dbb839dce873109a26d5b9b
Jacob Johnston <methodician@gmail.com>
2024-01-15 10:42:19 -0800

    ng added ng-fire basics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,39 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'general-ai-playground',
+          appId: '1:849308903389:web:e35bcf2c5f1db235b67834',
+          databaseURL:
+            'https://general-ai-playground-default-rtdb.firebaseio.com',
+          storageBucket: 'general-ai-playground.appspot.com',
+          apiKey: 'AIzaSyCqeeYdOPPymbyoNdhrEVVSZEWT_-iccRI',
+          authDomain: 'general-ai-playground.firebaseapp.com',
+          messagingSenderId: '849308903389',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideFunctions(() => getFunctions())),
+    importProvidersFrom(providePerformance(() => getPerformance())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+  ],
 };
